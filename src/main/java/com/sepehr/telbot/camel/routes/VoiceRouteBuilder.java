868d908386fe93833ac5a79e96f3327630fb3f1b
Sepehr79 <sepehrmsm1379@gmail.com>
2024-05-01 13:43:39 +0330

    enable voice processing

--- src/main/java/com/sepehr/telbot/camel/routes/VoiceRouteBuilder.java
+++ src/main/java/com/sepehr/telbot/camel/routes/VoiceRouteBuilder.java
@@ -76,17 +76,3 @@ public class VoiceRouteBuilder extends AbstractRouteBuilder {
         ;
     }
 }
-
-//curl -s -X POST   -H "Authorization: Bearer r8_3GVgrrydfN7KH6m1PcraJGV733e8HGI47jTGR"   -H "Content-Type: application/json"   -d '{
-//        "version": "4d50797290df275329f202e48c76360b3f22b08d28c196cbc54600319435f8d2",
-//        "input": {
-//        "audio": "https://api.telegram.org/file/bot6732521374:AAF8ogVAEVqmO70PmukFwQqccqRFeCWdEU4/voice/file_31.oga"
-//        }
-//        }'   https://api.replicate.com/v1/predictions
-
-//{"id":"06h9xhxewxrgc0cf6c9s0shqsm","model":"openai/whisper","version":"4d50797290df275329f202e48c76360b3f22b08d28c196cbc54600319435f8d2","input":{"audio":"https://api.telegram.org/file/bot6732521374:AAF8ogVAEVqmO70PmukFwQqccqRFeCWdEU4/voice/file_31.oga"},"logs":"","error":null,"status":"starting","created_at":"2024-05-01T07:34:39.079Z","urls":{"cancel":"https://api.replicate.com/v1/predictions/06h9xhxewxrgc0cf6c9s0shqsm/cancel","get":"https://api.replicate.com/v1/predictions/06h9xhxewxrgc0cf6c9s0shqsm"}}
-
-//curl -X GET   -H "Authorization: Bearer r8_3GVgrrydfN7KH6m1PcraJGV733e8HGI47jTGR"   -H "Content-Type: application/json" https://api.replicate.com/v1/predictions/87ghfs40esrga0cf6c89b9w7zg
-
-//{"id":"87ghfs40esrga0cf6c89b9w7zg","model":"openai/whisper","version":"4d50797290df275329f202e48c76360b3f22b08d28c196cbc54600319435f8d2","input":{"audio":"https://api.telegram.org/file/bot6732521374:AAF8ogVAEVqmO70PmukFwQqccqRFeCWdEU4/voice/file_31.oga"},"logs":"Transcribe with large-v3 model.\nDetected language: Persian\n  0%|          | 0/329 [00:00\u003c?, ?frames/s]\n100%|██████████| 329/329 [00:00\u003c00:00, 403.82frames/s]\n100%|██████████| 329/329 [00:00\u003c00:00, 403.72frames/s]\n","output":{"detected_language":"persian","segments":[{"avg_logprob":-0.25996342301368713,"compression_ratio":0.6538461538461539,"end":3.3000000000000003,"id":0,"no_speech_prob":0.004739189520478249,"seek":0,"start":0,"temperature":0,"text":" سلام خوبی","tokens":[50365,8608,37440,16490,37746,4135,50530]}],"transcription":" سلام خوبی","translation":null},"error":null,"status":"succeeded","created_at":"2024-05-01T07:31:10.582Z","started_at":"2024-05-01T07:31:29.283918Z","completed_at":"2024-05-01T07:31:31.267828Z","urls":{"cancel":"https://api.replicate.com/v1/predictions/87ghfs40esrga0cf6c89b9w7zg/cancel","get":"https://api.replicate.com/v1/predictions/87ghfs40esrga0cf6c89b9w7zg"},"metrics":{"predict_time":1.98391}}
-
