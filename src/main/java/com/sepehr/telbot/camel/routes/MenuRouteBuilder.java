42dc1a6ddbaeed7f4d545f942dd4dd7dc5d5ce34
Sepehr79 <sepehrmsm1379@gmail.com>
2024-05-01 22:53:42 +0330

    update start page

--- src/main/java/com/sepehr/telbot/camel/routes/MenuRouteBuilder.java
+++ src/main/java/com/sepehr/telbot/camel/routes/MenuRouteBuilder.java
@@ -4,6 +4,7 @@ import com.sepehr.telbot.config.ApplicationConfiguration;
 import org.apache.camel.component.telegram.model.InlineKeyboardButton;
 import org.apache.camel.component.telegram.model.InlineKeyboardMarkup;
 import org.apache.camel.component.telegram.model.OutgoingMessage;
+import org.apache.camel.component.telegram.model.OutgoingTextMessage;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -24,10 +25,11 @@ public class MenuRouteBuilder extends AbstractRouteBuilder {
                             InlineKeyboardMarkup.builder()
                                     .addRow(List.of(InlineKeyboardButton.builder().text("حمایت از ما").callbackData("/donate").build()))
                                     .build();
-                    final OutgoingMessage outgoingMessage = getOutGoingTextMessageBuilder(exchange, "سلام! این ربات بهت کمک میکنه که از طریق تلگرام با ChatGPT صحبت کنی.\n\n" +
+                    final OutgoingTextMessage outgoingMessage = (OutgoingTextMessage) getOutGoingTextMessageBuilder(exchange, "سلام! این ربات بهت کمک میکنه که از طریق تلگرام با ChatGPT صحبت کنی.\n\n" +
                             "در حال حاضر برای پاسخ به شما از مدل gpt-3.5-turbo استفاده میشه که میتونین مستقیم از طریق:\n https://chat.openai.com\n بهش دسترسی پیدا کنید.\n\n" +
-                            "قابلیت ها:\n1. سوالات خودتون رو بپرسید تا ربات اونها را به ChatGPT ارسال کنه و پاسخش رو به شما برگردونه.\n2. همچنین میتونین این ربات رو به گروه ها اضافه کنید در این صورت برای اینکه ربات رو صدا کنید کافیه اول پیامتون // قرار بدید.\n\n تلاش میکنیم در آینده امکانات بیشتری به این ربات اضافه کنیم.", replyKeyboardMarkup);
+                            "قابلیت ها:\n1. سوالات خودتون رو به صورت متنی یا صوتی بپرسید تا ربات اونها را به ChatGPT ارسال کنه و پاسخش رو به شما برگردونه.\n2. همچنین میتونین این ربات رو به گروه ها اضافه کنید در این صورت برای اینکه ربات رو صدا کنید کافیه اول پیامتون // قرار بدید.\n\nبرخی از سوال هایی که میتونین بپرسین:\n1. `مهمترین اصول مقاله نویسی را به من بگو`\n2. `چه کسی نخستین بار قاره آمریکا را فتح کرد`\n2. `الگوریتم بلمن فورد را با پایتون پیاده سازی کن`\n\n تلاش میکنیم در آینده امکانات بیشتری به این ربات اضافه کنیم.", replyKeyboardMarkup);
 
+                    outgoingMessage.setParseMode("markdown");
                     exchange.getMessage().setBody(outgoingMessage);
                 });
     }
